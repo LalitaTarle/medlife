4f2da423b499e3d8c3a8b25cd5fdc8153d84fca6
LalitaTarle <lalitatarle7@gmail.com>
2023-03-21 11:51:51 +0530

    top-deals implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { CoreModule } from './core/core.module';
+import { HomeComponent } from './components/home/home.component';
+import { MedicineHomeComponent } from './components/medicine-home/medicine-home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { TopDealsComponent } from './components/top-deals/top-deals.component';
+import{ HttpClient, HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MedicineHomeComponent,
+    TopDealsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule, 
+    BrowserAnimationsModule,
+    CarouselModule,
+    HttpClientModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
